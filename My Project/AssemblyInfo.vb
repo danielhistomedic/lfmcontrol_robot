dc73e6e02677bf3b498e74a708637b2a15feefe6
daniel <daniel@histomedic.mx>
2026-09-23 07:47:51 -0600

    20260923 0747

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' mediante el asterisco ('*'), como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.3.1.274")> 
-<Assembly: AssemblyFileVersion("2.3.1.274")> 
+<Assembly: AssemblyVersion("2.3.1.276")> 
+<Assembly: AssemblyFileVersion("2.3.1.276")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("")> 
